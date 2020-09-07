6b0ac0c645043ffb7c32c57d485aba3065ac7ab3
budo <budo-k@jobcorwn.co.jp>
2020-09-07 15:46:31 +0900

    addreciber

--- AlarmNoti/app/src/main/java/com/example/alarmnoti/AlarmReceiver.java
+++ AlarmNoti/app/src/main/java/com/example/alarmnoti/AlarmReceiver.java
@@ -2,12 +2,14 @@ package com.example.alarmnoti;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.NotificationChannel;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 public class AlarmReceiver extends BroadcastReceiver {
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
 
 
     @Override
@@ -19,7 +21,16 @@ public class AlarmReceiver extends BroadcastReceiver {
                 new Intent(context, MainActivity.class);
         PendingIntent contentIntent =
                 PendingIntent.getActivity(context, 0, bootIntent, 0);
+
         Notification.Builder builder = new Notification.Builder(context);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+
+            builder.setChannelId(NOTIFICATION_CHANNEL_ID);
+            myNotification.createNotificationChannel(notificationChannel);
+        }
         builder.setSmallIcon(android.R.drawable.ic_dialog_info)
                 .setContentTitle("時間ですよ")
                 .setContentText(receivedIntent.getCharSequenceExtra("todo"))
